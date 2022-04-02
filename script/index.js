32e592d5bda34456d7d2ec2872cabbe66e06dc91
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-04-02 09:54:09 -0400

    html icon fix

--- script/index.js
+++ script/index.js
@@ -31,7 +31,7 @@ function button(c){
     '});'+
     'headingElement.innerHTML=\'\';headingElement.innerHTML=headingContent;'+
     'search();">'+
-        '<span class="h5 m-0 p-0">🢪</span> <b>back</b>';
+        '<span class="h5 m-0 p-0"><i class="bi bi-arrow-left"></i></span> <b>back</b>';
     parentElement.innerHTML = '';
     entriesData.entries.forEach(dt => {
         if(c ==  dt.Category){
